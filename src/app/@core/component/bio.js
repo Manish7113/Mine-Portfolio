11dfa8d3b8d4432e715fd2a0cc65de34ef3d6625
Manish7113 <kumawatji7113@gmail.com>
2024-07-12 13:08:40 +0530

    updates

--- src/app/@core/component/bio.js
+++ src/app/@core/component/bio.js
@@ -7,7 +7,7 @@ export default function Bio() {
                 <p className="sectionHeading fs-xll roboto-regular  ">About Me</p>
                 
                 <p className="roboto-regular fs-lgg txt-center ">
-                   <span className="fs-xll red">I</span> <span className="">turn</span>  ideas into websites! Think of me as a web architect. I use languages like HTML, CSS, and JavaScript (think building blocks) to craft websites, landing pages (fancy intro pages), and even admin panels (like the control room of a website). I also throw in React and Next.js for extra speed and efficiency. Basically, I'm the guy behind the scenes making sure websites look awesome and work flawlessly
+                   <span className="fs-xll red">I</span> <span className="">turn</span>  ideas into websites! Think of me as a web architect. I use languages like HTML, CSS, and JavaScript think building blocks to craft websites, landing pages fancy intro pages, and even admin panels like the control room of a website. I also throw in React and Next.js for extra speed and efficiency. Basically, I'm the guy behind the scenes making sure websites look awesome and work flawlessly
                 </p>
 
             </div>
